fddcff9930765d65e241bb4c67d44994a92ffeb9
Glitch (rps-metafs) <none>
2021-12-01 23:07:04 +0000

    👳📒 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 let userInput = $("input").val();
 $(".play").text(userInput);
-});
 
+$(".play").click(function() {
+    let fortune = Math.ceil(Math.random() * 4);
+    console.log(fortune)
+});
+ if (fortune > .3333333333){
+ $(".computerChoice").append("rock");
+    }
